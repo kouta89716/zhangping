17577515206f4255681720ad4d6e0cafc6a91c3f
kouta89716 <499676664@qq.com>
2018-06-04 19:32:19 +0800

    update

---
+++ zhangping/Form1.Designer.cs
@@ -0,0 +1,221 @@
+﻿namespace zhangping
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.label2 = new System.Windows.Forms.Label();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.progressBar2 = new System.Windows.Forms.ProgressBar();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(26, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(155, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "指定XML的目录";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(187, 17);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(47, 12);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "路径...";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(697, 12);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "开始转换";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // listView1
+            // 
+            this.listView1.Location = new System.Drawing.Point(26, 64);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(746, 158);
+            this.listView1.TabIndex = 4;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.List;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(24, 45);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(65, 12);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "输出信息：";
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(26, 284);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(746, 23);
+            this.progressBar1.TabIndex = 6;
+            // 
+            // label3
+            // 
+            this.label3.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label3.Location = new System.Drawing.Point(26, 235);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(748, 2);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "label3";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(677, 255);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(95, 23);
+            this.button3.TabIndex = 8;
+            this.button3.Text = "生成出库表格";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(677, 313);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(95, 23);
+            this.button4.TabIndex = 9;
+            this.button4.Text = "生成入库表格";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // progressBar2
+            // 
+            this.progressBar2.Location = new System.Drawing.Point(26, 342);
+            this.progressBar2.Name = "progressBar2";
+            this.progressBar2.Size = new System.Drawing.Size(746, 23);
+            this.progressBar2.TabIndex = 10;
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(26, 255);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(132, 23);
+            this.button5.TabIndex = 11;
+            this.button5.Text = "选择出库xml文件夹";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(26, 312);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(132, 23);
+            this.button6.TabIndex = 12;
+            this.button6.Text = "选择入库xml文件夹";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(164, 260);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(89, 12);
+            this.label4.TabIndex = 13;
+            this.label4.Text = "出库xml路径...";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(164, 318);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(89, 12);
+            this.label5.TabIndex = 14;
+            this.label5.Text = "入库xml路径...";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.progressBar2);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "做起来了找我当IT吧";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.ProgressBar progressBar2;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ zhangping/Form1.cs
@@ -0,0 +1,210 @@
+﻿using System;
+using System.IO;
+using System.Windows.Forms;
+using System.Xml;
+namespace zhangping
+{
+    public partial class Form1 : Form
+    {
+        public static string Dir;
+        public static string cDir;
+        public static string rDir;
+       
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog dialog = new FolderBrowserDialog();
+            dialog.Description = "请选择文件路径";
+            if (dialog.ShowDialog() == DialogResult.OK)
+            {
+                string foldPath = dialog.SelectedPath;
+                this.label1.Text = "已选择文件夹:" + foldPath;
+                Dir = foldPath;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {   
+            try {
+                if (!Directory.Exists(Dir+"\\out"))   
+                {
+                    Directory.CreateDirectory(Dir + "\\out");   
+                }
+                DirectoryInfo dir = new DirectoryInfo(@"" + Dir + "");
+                int i = 0;
+                foreach (FileInfo dChild in dir.GetFiles("*.xml"))
+                {
+                    try
+                    {
+                        XmlDocument xmlDoc = new XmlDocument();
+                        xmlDoc.Load(dChild.FullName);
+                        //xmlDoc.SelectSingleNode("WMSANS/Status").InnerText = "FIN";
+                        XmlDocument newXmlDoc = new XmlDocument();
+                        newXmlDoc.AppendChild(newXmlDoc.CreateXmlDeclaration("1.0", "utf-8", ""));
+                        XmlElement xe = newXmlDoc.CreateElement("WMSANS");
+                        newXmlDoc.AppendChild(xe);
+                        XmlElement MessageSenderIdentifier = newXmlDoc.CreateElement("MessageSenderIdentifier");
+                        XmlElement MessageSenderName = newXmlDoc.CreateElement("MessageSenderName");
+                        XmlElement MessageReceiverIdentifier = newXmlDoc.CreateElement("MessageReceiverIdentifier");
+                        XmlElement MessageReceiverName = newXmlDoc.CreateElement("MessageReceiverName");
+                        XmlElement MessageTypeIdentifier = newXmlDoc.CreateElement("MessageTypeIdentifier");
+                        XmlElement DocumentNumber = newXmlDoc.CreateElement("DocumentNumber");
+                        XmlElement DocumentANSTime = newXmlDoc.CreateElement("DocumentANSDateTime");
+                        XmlElement Status = newXmlDoc.CreateElement("Status");
+                        XmlElement RefNo = newXmlDoc.CreateElement("RefNo");
+                        XmlElement Remark = newXmlDoc.CreateElement("Remark");
+                        if (xmlDoc.SelectSingleNode("WMSRECV") != null) {
+                            MessageSenderIdentifier.InnerText = xmlDoc.SelectSingleNode("WMSRECV/Header/MessageSenderIdentifier").InnerText;
+                            MessageSenderName.InnerText = xmlDoc.SelectSingleNode("WMSRECV/Header/MessageSenderName").InnerText;
+                            MessageReceiverIdentifier.InnerText = xmlDoc.SelectSingleNode("WMSRECV/Header/MessageReceiverIdentifier").InnerText;
+                            MessageReceiverName.InnerText = xmlDoc.SelectSingleNode("WMSRECV/Header/MessageReceiverName").InnerText;
+                            MessageTypeIdentifier.InnerText = xmlDoc.SelectSingleNode("WMSRECV/Header/MessageTypeIdentifier").InnerText;
+                            DocumentNumber.InnerText = xmlDoc.SelectSingleNode("WMSRECV/Header/DocumentNumber").InnerText;
+                            DocumentANSTime.InnerText = xmlDoc.SelectSingleNode("WMSRECV/Header/DocumentDateTime").InnerText;
+                            RefNo.InnerText = xmlDoc.SelectSingleNode("WMSRECV/Header/InvoiceNo").InnerText;
+                            Remark.InnerText = xmlDoc.SelectSingleNode("WMSRECV/Header/Remark").InnerText;
+                        }
+                        if (xmlDoc.SelectSingleNode("WMSRELS") != null) {
+                            MessageSenderIdentifier.InnerText = xmlDoc.SelectSingleNode("WMSRELS/Header/MessageSenderIdentifier").InnerText;
+                            MessageSenderName.InnerText = xmlDoc.SelectSingleNode("WMSRELS/Header/MessageSenderName").InnerText;
+                            MessageReceiverIdentifier.InnerText = xmlDoc.SelectSingleNode("WMSRELS/Header/MessageReceiverIdentifier").InnerText;
+                            MessageReceiverName.InnerText = xmlDoc.SelectSingleNode("WMSRELS/Header/MessageReceiverName").InnerText;
+                            MessageTypeIdentifier.InnerText = xmlDoc.SelectSingleNode("WMSRELS/Header/MessageTypeIdentifier").InnerText;
+                            DocumentNumber.InnerText = xmlDoc.SelectSingleNode("WMSRELS/Header/DocumentNumber").InnerText;
+                            DocumentANSTime.InnerText = xmlDoc.SelectSingleNode("WMSRELS/Header/DocumentDateTime").InnerText;
+                            RefNo.InnerText = xmlDoc.SelectSingleNode("WMSRELS/Header/InvoiceNo").InnerText;
+                            Remark.InnerText = xmlDoc.SelectSingleNode("WMSRELS/Header/Remark").InnerText;
+                        }
+                        Status.InnerText = "FIN";
+                        xe.AppendChild(MessageSenderIdentifier);
+                        xe.AppendChild(MessageSenderName);
+                        xe.AppendChild(MessageReceiverIdentifier);
+                        xe.AppendChild(MessageReceiverName);
+                        xe.AppendChild(MessageTypeIdentifier);
+                        xe.AppendChild(DocumentNumber);
+                        xe.AppendChild(DocumentANSTime);
+                        xe.AppendChild(Status);
+                        xe.AppendChild(RefNo);
+                        xe.AppendChild(Remark);
+                        if (xmlDoc.SelectSingleNode("WMSRECV") != null)
+                        {
+                            newXmlDoc.Save(Dir + "\\out\\" + xmlDoc.SelectSingleNode("WMSRECV/Header/InvoiceNo").InnerText + "_REC.xml");
+                            newXmlDoc.Save(Dir + "\\out\\" + xmlDoc.SelectSingleNode("WMSRECV/Header/InvoiceNo").InnerText + "_FIN.xml");
+                        }
+                        if (xmlDoc.SelectSingleNode("WMSRELS") != null)
+                        {
+                            newXmlDoc.Save(Dir + "\\out\\" + xmlDoc.SelectSingleNode("WMSRELS/Header/InvoiceNo").InnerText + "_REC.xml");
+                            newXmlDoc.Save(Dir + "\\out\\" + xmlDoc.SelectSingleNode("WMSRELS/Header/InvoiceNo").InnerText + "_FIN.xml");
+                        }
+                        listView1.Items.Add(dChild.FullName + "========【成功】");
+                        listView1.LabelWrap = true;
+                        listView1.EnsureVisible(i);
+                        i++;
+                    }
+                    catch {
+                        listView1.Items.Add(dChild.FullName + "========【失败】");
+                        listView1.LabelWrap = true;
+                        listView1.EnsureVisible(i);
+                        i++;
+                    }
+                    // Response.Write(dChild.Name + "<BR>");//打印文件名
+                    // Response.Write(dChild.FullName + "<BR>");//打印路径和文件名
+                }
+           } catch {
+               MessageBox.Show("第一步要先选路径!");
+           }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                progressBar1.Value = 0;
+                progressBar1.Step = 1;
+                if (!Directory.Exists(cDir + "\\out")){
+                    Directory.CreateDirectory(cDir + "\\out");
+                }
+                DirectoryInfo dir = new DirectoryInfo(@"" + cDir + "");
+                XmlDocument newXmlDoc = new XmlDocument();
+                newXmlDoc.AppendChild(newXmlDoc.CreateXmlDeclaration("1.0", "utf-8", ""));
+                XmlElement xe = newXmlDoc.CreateElement("WMSRELSLIST");
+                newXmlDoc.AppendChild(xe);
+                progressBar1.Maximum = dir.GetFiles("*.xml").Length;
+                foreach (FileInfo dChild in dir.GetFiles("*.xml")){
+                    XmlDocument xmlDoc = new XmlDocument();
+                    xmlDoc.Load(dChild.FullName);
+                    XmlElement node = xmlDoc.DocumentElement;
+                    XmlNode xnDesi = newXmlDoc.ImportNode(xmlDoc.DocumentElement, true);
+                    newXmlDoc.DocumentElement.AppendChild(xnDesi);
+                    progressBar1.Value += progressBar1.Step;
+                }
+                newXmlDoc.Save(cDir + "\\out\\out.xml");
+                newXmlDoc.Save(cDir + "\\out\\out.xls");
+            }
+            catch{
+                MessageBox.Show("第一步要先选路径!");
+            }
+        }
+        private void button4_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (!Directory.Exists(rDir + "\\out"))
+                {
+                    Directory.CreateDirectory(rDir + "\\out");
+                }
+                DirectoryInfo dir = new DirectoryInfo(@"" + rDir + "");
+                int i = 0;
+                XmlDocument newXmlDoc = new XmlDocument();
+                newXmlDoc.AppendChild(newXmlDoc.CreateXmlDeclaration("1.0", "utf-8", ""));
+                XmlElement xe = newXmlDoc.CreateElement("WMSRELSLIST");
+                newXmlDoc.AppendChild(xe);
+                foreach (FileInfo dChild in dir.GetFiles("*.xml"))
+                {
+                    i++;
+                    XmlDocument xmlDoc = new XmlDocument();
+                    xmlDoc.Load(dChild.FullName);
+                    XmlElement node = xmlDoc.DocumentElement;
+                    XmlNode xnDesi = newXmlDoc.ImportNode(xmlDoc.DocumentElement, true);
+                    newXmlDoc.DocumentElement.AppendChild(xnDesi);
+                }
+                newXmlDoc.Save(rDir + "\\out\\out.xml");
+                newXmlDoc.Save(rDir + "\\out\\out.xls");
+            }
+            catch
+            {
+                MessageBox.Show("第一步要先选路径!");
+            }
+        }
+        private void button5_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog dialog = new FolderBrowserDialog();
+            dialog.Description = "请选择出库文件路径";
+            if (dialog.ShowDialog() == DialogResult.OK)
+            {
+                string foldPath = dialog.SelectedPath;
+                this.label4.Text = "已选择文件夹:" + foldPath;
+                cDir = foldPath;
+            }
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog dialog = new FolderBrowserDialog();
+            dialog.Description = "请选择入库文件路径";
+            if (dialog.ShowDialog() == DialogResult.OK)
+            {
+                string foldPath = dialog.SelectedPath;
+                this.label5.Text = "已选择文件夹:" + foldPath;
+                rDir = foldPath;
+            }
+        }
+
+       
+    }
+}
+
